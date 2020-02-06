838e10151875435a204cf384952317d697ac2c33
Xiangyu.Li <562936406@qq.com>
2020-02-06 14:58:51 +0800

    calendar update 0206

--- app/calendar/views.py
+++ app/calendar/views.py
@@ -1,7 +1,15 @@
 from . import calendar
 from flask import render_template
+from flask_login import login_required
 
 
 @calendar.route('/')
-def calendar():
+@login_required
+def calendar_home():
     return render_template('calendar/calendar.html')
+
+
+@calendar.route('/base_page')
+@login_required
+def calendar_base():
+    return render_template('calendar/calendar_base.html')
