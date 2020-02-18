c26c95b2d03f0ea10d62260c318ba85c095b7819
black <78845906@qq.com>
2020-02-18 10:24:29 +0800

    增加微信蓝图

--- app/calendar/views.py
+++ app/calendar/views.py
@@ -8,6 +8,12 @@ import json
 from app import db
 from app.util import log, comments
 
+from random import randrange
+from pyecharts import options as opts
+from pyecharts.charts import Bar,Geo
+from pyecharts.faker import Collector, Faker
+from pyecharts.globals import ChartType, SymbolType
+
 
 @calendar.route('/')
 @login_required
@@ -50,3 +56,42 @@ def store_user_calendar():
     # log(id, TITLE, START, END, UID)
     return_dict = {'return_code': '200', 'return_info': '', 'result': False}
     return json.dumps(return_dict, ensure_ascii=False)
+
+
+# 以下为ly作测试使用
+C = Collector()
+@C.funcs
+def geo_lines() -> Geo:
+    c = (
+        Geo()
+        .add_schema(maptype="china")
+        .add(
+            "",
+            [("广州", 55), ("北京", 66), ("杭州", 77), ("重庆", 88)],
+            type_=ChartType.EFFECT_SCATTER,
+            color="white",
+        )
+        .add(
+            "geo",
+            [("广州", "上海"), ("广州", "北京"), ("广州", "杭州"), ("广州", "重庆")],
+            type_=ChartType.LINES,
+            effect_opts=opts.EffectOpts(
+                symbol=SymbolType.ARROW, symbol_size=6, color="blue"
+            ),
+            linestyle_opts=opts.LineStyleOpts(curve=0.2),
+        )
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(title_opts=opts.TitleOpts(title="Geo-Lines"))
+    )
+    return c
+
+@calendar.route('/test')
+@login_required
+def calendarHome():
+    return render_template('calendar/calendar_ly.html')
+
+
+@calendar.route("/barChart")
+def get_bar_chart():
+    c = geo_lines()
+    return c.dump_options_with_quotes()
