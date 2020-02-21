233d36f84ec30910b074dafa7948f5866da96218
black <78845906@qq.com>
2020-02-21 15:32:54 +0800

    修改链接库功能

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,4 +1,6 @@
-from flask import render_template
+import json
+
+from flask import render_template, request
 from flask_login import login_required
 
 from app import db
@@ -18,18 +20,41 @@ def home():
 @admin.route('/href_lib')
 @login_required
 def hrefLib():
-    # 区县的链接数据
+    return render_template('admin/href_lib.html')
+
+
+@admin.route('/api/href_lib/region_data')
+@login_required
+def hrefLibRegionData():
+    return_dict = {'return_code': '200', 'return_info': '', 'result': True}
+    if request.args is None:
+        return_dict['return_code'] = '50004'
+        return_dict['return_info'] = '传入参数为空'
+        return json.dumps(return_dict, ensure_ascii=False)  # ensure_ascii=False才能输出中文
+    # 获取传入的参数
+    get_data = request.args.to_dict()
+    region_name = get_data.get("region_name")
+    print(region_name)
     r_region = DICT_REGION.query.join(DICT_HREF, DICT_REGION.REGION_CODE == DICT_HREF.REGION_CODE).with_entities(
-        DICT_REGION.REGION_CODE, DICT_REGION.REGION_NAME, DICT_HREF.HREF,DICT_HREF.NAME).filter().all()
-    region_dict = {}
+        DICT_REGION.REGION_CODE, DICT_REGION.REGION_NAME, DICT_HREF.HREF, DICT_HREF.NAME).filter(
+        DICT_REGION.REGION_NAME == region_name).all()
+    region_data = {}
     for i in r_region:
         region_name_href_dict = {}
-        if i.REGION_CODE in region_dict:
-            value_list = region_dict[i.REGION_CODE]
+        if i.REGION_NAME in region_data:
+            value_list = region_data[i.REGION_NAME]
         else:
             value_list = []
         region_name_href_dict[i.NAME] = i.HREF
         value_list.append(region_name_href_dict)
-        region_dict[i.REGION_CODE] = value_list
-    print(region_dict)
-    return render_template('admin/href_lib.html')
+        region_data[i.REGION_NAME] = value_list
+
+    if return_dict:
+        return_dict['return_info'] = region_data
+    else:
+        return_dict['return_code'] = '1000'
+        return_dict['return_info'] = '未查询到相关数据！'
+        return_dict['result'] = False
+    print(return_dict)
+
+    return json.dumps(return_dict, ensure_ascii=False)
