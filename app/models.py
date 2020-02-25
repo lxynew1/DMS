215e80276a78fc24a3f10b0d8ddb7c59ae7283a0
black <78845906@qq.com>
2020-02-25 09:56:38 +0800

    修改字段长度和标题

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class Role(db.Model):
 class LAND_SELL_INFO(db.Model):
     __tablename__ = 'LAND_SELL_INFO'
     FID = db.Column(db.String(50), primary_key=True, comment='随机UUID')
-    NOTICE_NUM = db.Column(db.String(50), unique=True, nullable=False, comment='公告编号')
+    NOTICE_NUM = db.Column(db.String(100), unique=True, nullable=False, comment='公告编号')
     LAND_LOCATION = db.Column(db.String(255), nullable=False, comment='位置')
     TOTAL_AREA = db.Column(db.Float, nullable=False, comment='总面积（平方米）')
     CONSTRUCTION_AREA = db.Column(db.Float, nullable=True, comment='建设用地面积（平方米）')
@@ -58,7 +58,7 @@ class LAND_SELL_INFO(db.Model):
     NOTICE_USE = db.Column(db.String(100), nullable=False, comment='公告用途')
     PLAN_USE = db.Column(db.String(100), nullable=False, comment='规划用途')
     PLAN_USE_CUSTOM = db.Column(db.String(50), nullable=False, comment='自定义用途')
-    FLOOR_AREA_RATIO = db.Column(db.String(50), nullable=False, comment='容积率')
+    FLOOR_AREA_RATIO = db.Column(db.String(100), nullable=False, comment='容积率')
     GREENING_RATE = db.Column(db.String(50), nullable=False, comment='绿化率')
     BUSSINESS_PROPORTION = db.Column(db.String(50), nullable=False, comment='商业比例')
     BUILDING_DENSITY = db.Column(db.String(50), nullable=False, comment='建筑密度')
