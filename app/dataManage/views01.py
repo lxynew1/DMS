f6e991decd1035cf60fcc4b620c6af3dcd20ae27
Xiangyu.Li <562936406@qq.com>
2020-03-04 13:49:39 +0800

    datamanage 查询更新02 0304

--- app/dataManage/views01.py
+++ app/dataManage/views01.py
@@ -57,7 +57,20 @@ def objSearchValue(num: int, vlaue: str, data: List[str], obj):
     for i in obj:
         if str(i).__contains__(vlaue):
             list = [
-                str(num) + " <a href='/datamanage/land_sell_deal?fid=" + obj.FID + "' target='blank' class='up btn btn-default btn-xs'><i class='fa fa-arrow-up'></i> 编辑</a>",
+                '''
+                <button type="button" class="btn btn-primary waves-effect waves-light">Dropdown</button>
+                <button type="button" class="btn btn-primary split-btn-primary dropdown-toggle dropdown-toggle-split waves-effect waves-light" data-toggle="dropdown">
+                    <span class="caret"></span>
+                  </button>
+                  <div class="dropdown-menu">
+                    <a href="javaScript:void();" class="dropdown-item">Action</a>
+                    <a href="javaScript:void();" class="dropdown-item">Another action</a>
+                    <a href="javaScript:void();" class="dropdown-item">Something else here</a>
+                    <div class="dropdown-divider"></div>
+                    <a href="javaScript:void();" class="dropdown-item">Separated link</a>
+                  </div>
+                  ''',
+                # str(num) + " <a href='/datamanage/land_sell_deal?fid=" + obj.FID + "' target='blank' class='up btn btn-default btn-xs'><i class='fa fa-arrow-up'></i> 编辑</a>",
                 obj.REGION_NAME,  # 区县代码
                 obj.NOTICE_NUM,  # 公告编号
                 obj.LAND_LOCATION,  # 位置
